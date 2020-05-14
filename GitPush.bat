33c70d561221456c44f2e02282400a9fee0ebc78
ThucTran <thucspc@gmail.com>
2020-05-14 09:01:54 +0700

    20205/02_090149

--- GitPush.bat
+++ GitPush.bat
@@ -20,6 +20,6 @@ call git config --global user.name "ThucTran"
 call git pull origin master
 call git add .
 call git commit -am %dtStamp%
+PAUSE
 call git push origin master
 
-PAUSE
---
+++ GitReset.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call git reset --hard HEAD~1
