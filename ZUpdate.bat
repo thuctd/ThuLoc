30fd93477fb8e8ddd1f2a87167a7b314cb73535d
ThucTran <thucspc@gmail.com>
2020-05-30 20:38:33 +0700

    30/05/2020_20:38:33.75

--- ZUpdate.bat
+++ ZUpdate.bat
@@ -1,18 +1,13 @@
 
-
 @ECHO OFF
 @echo Started: %date% %time%
-.
-.
-.
-@echo Completed: %date% %time%
 
 
 call git config --global user.email "thucspc@gmail.com"
 call git config --global user.name "ThucTran"
 call git pull origin master
 call git add .
-call git commit -am %mydate%_%mytime%
+call git commit -am %date%_%time%
 PAUSE
 call git push origin master -f
 PAUSE
